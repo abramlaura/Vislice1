11fce232697dfdfb49bf0b515e585d08f3c45f04
Saša Prelog <sasa.prelog6@gmail.com>
2019-05-14 18:49:30 +0200

    ert

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -1,2 +1,17 @@
 def izpis_igre(igra):
-    
+return  """===================================================================
+
+{geslo}
+Napacne crke : {napacne_crke}
+Ugibaš še : {število} -krat.
+==============================================""".format(
+    geslo=igra.pravilni_del_gesla(),
+    crke=igra.nepravilni_ugibi()
+    stevilo=model.STEVILO_DOVOLJENIH_NAPAK - igra.stevilo_napak())
+
+
+def izpis_zmaga(igra):
+    return 'Čestitam, uganil/a si geslo {}.'.format(igra)
+
+def izpis_poraza(igra):
+    return 'Več sreče prihodnjič.'.format(igra)
